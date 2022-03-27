1e57bcf858b7fc49e6f6fc9ce6a65e07457e5f91
rinatpc <rinat2511@gmail.com>
2022-03-27 16:32:10 +0500

    1 finished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+  Route::get('/', [MainController::class, 'index'])->name('admin.index');
 });
